0cb0e6c8837010423b5524dd11ad8ae8dd7260ed
JLJDZZY <1443813447@qq.com>
2025-08-23 20:23:13 +0800

    8.18

--- for .c
+++ for .c
@@ -14,8 +14,9 @@ int main() {
     int fact=1;
     int i=1;
 
-    for (i=1;i<=n;i++) {
+    for (i=1;i<=n;i++) {//  i=1为初始条件；i<=n为判断条件，i++为循环要执行的动作
         fact *= i;
+        break;//遇到break会跳出循环
     }
 
 printf("%d!= %d",n,fact);
